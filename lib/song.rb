27e6f342ab8e932a5052fc98c53f8909fd1976bd
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-19 17:01:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song 
   attr_accessor :name, :artist, :genre
-    @@song_count = 0
+    @@count = 0
 
   def initialize (name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
-    
   end
+  
+  def 
 
 
 end
